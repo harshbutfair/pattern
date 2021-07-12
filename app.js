d8db69974bd5a36e67ee1989ac75f51e6fea85a9
harshbutfair <harsh.narayan@yahoo.com>
2021-07-13 01:49:34 +0530

    Added factory pattern

--- app.js
+++ app.js
@@ -60,30 +60,83 @@
 
 // Singleton Pattern
 
-const Singleton =(function(){
-    let instance;
+// const Singleton =(function(){
+//     let instance;
 
-    function createInstance() {
-        const object = new Object({name: 'Brad'});
-        return object;
-    }
+//     function createInstance() {
+//         const object = new Object({name: 'Brad'});
+//         return object;
+//     }
 
-    return {
-        getInstance: function() {
-            if(!instance){
-                instance = createInstance();
-            }
-            return instance;
-        }
-    }
-})();
+//     return {
+//         getInstance: function() {
+//             if(!instance){
+//                 instance = createInstance();
+//             }
+//             return instance;
+//         }
+//     }
+// })();
 
-const instanceA = Singleton.getInstance();
-const instanceB = Singleton.getInstance();
+// const instanceA = Singleton.getInstance();
+// const instanceB = Singleton.getInstance();
 
-console.log(instanceA);
-console.log(instanceB);
+// console.log(instanceA);
+// console.log(instanceB);
 
-console.log(instanceA === instanceB);
+// console.log(instanceA === instanceB);
 
 // Cannot have more than one instance with Singleton pattern
+
+
+function memberFactory() {
+        this.createMember = function(name, type) {
+            let member;
+
+            if(type === 'simple'){
+                member = new SimpleMembership(name);
+            } else if (type === 'standard') {
+                member = new StandardMembership(name);
+            } else if (type === 'super') {
+                member = new SuperMembership(name);
+            }
+
+        member.type = type;
+
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`)
+        }
+
+        return member;
+    }
+}
+
+const SimpleMembership = function(name) {
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMembership = function(name) {
+    this.name = name;
+    this.cost = '$15';
+}
+
+const SuperMembership = function(name) {
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+const factory = new memberFactory();
+
+members.push(factory.createMember('John Doe', 'simple'));
+members.push(factory.createMember('Doe Doe', 'super'));
+members.push(factory.createMember('John John', 'super'));
+members.push(factory.createMember('Doe John', 'standard'));
+
+
+// console.log(members);
+
+members.forEach(function(member){
+    member.define();
+})
