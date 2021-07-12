5defe655904d8ea9e84f2aa3efd6fe1bf70ed84b
harshbutfair <harsh.narayan@yahoo.com>
2021-07-12 23:51:48 +0530

    Added Singleton Pattern

--- app.js
+++ app.js
@@ -32,26 +32,58 @@
 // console.log(UICtrl.text);
 
 // REVEALING MODULE PATTERN
-const itemCtrl = (function(){
-    let data = [];
+// const itemCtrl = (function(){
+//     let data = [];
 
-    function add(item){
-        data.push(item);
-        console.log('Item Added...');
-    }
+//     function add(item){
+//         data.push(item);
+//         console.log('Item Added...');
+//     }
 
-    function get(id){
-        return data.find(item => {
-            return item.id === id;
-        })
+//     function get(id){
+//         return data.find(item => {
+//             return item.id === id;
+//         })
+//     }
+
+//     return {
+//         add: add,
+//         get: get
+//     }
+// })();
+
+// itemCtrl.add({id : 1, name: 'John'});
+// itemCtrl.add({id : 2, name: 'Mark'});
+// console.log(itemCtrl.get(1));   
+
+// ----------------------------------------------------------------------------------------//
+
+// Singleton Pattern
+
+const Singleton =(function(){
+    let instance;
+
+    function createInstance() {
+        const object = new Object({name: 'Brad'});
+        return object;
     }
 
     return {
-        add: add,
-        get: get
+        getInstance: function() {
+            if(!instance){
+                instance = createInstance();
+            }
+            return instance;
+        }
     }
 })();
 
-itemCtrl.add({id : 1, name: 'John'});
-itemCtrl.add({id : 2, name: 'Mark'});
-console.log(itemCtrl.get(1));   
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
+
+console.log(instanceA);
+console.log(instanceB);
+
+console.log(instanceA === instanceB);
+
+// Cannot have more than one instance with Singleton pattern
