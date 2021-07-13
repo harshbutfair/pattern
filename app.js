6dc2447f374f8066d441c64e97d8dd48daf1b678
harshbutfair <harsh.narayan@yahoo.com>
2021-07-13 23:52:01 +0530

    Added Oberserver pattern

--- app.js
+++ app.js
@@ -88,55 +88,116 @@
 
 // Cannot have more than one instance with Singleton pattern
 
+//-------------------------------------------------------------------------------------------//
 
-function memberFactory() {
-        this.createMember = function(name, type) {
-            let member;
+// Factory Pattern
 
-            if(type === 'simple'){
-                member = new SimpleMembership(name);
-            } else if (type === 'standard') {
-                member = new StandardMembership(name);
-            } else if (type === 'super') {
-                member = new SuperMembership(name);
+// function memberFactory() {
+//         this.createMember = function(name, type) {
+//             let member;
+
+//             if(type === 'simple'){
+//                 member = new SimpleMembership(name);
+//             } else if (type === 'standard') {
+//                 member = new StandardMembership(name);
+//             } else if (type === 'super') {
+//                 member = new SuperMembership(name);
+//             }
+
+//         member.type = type;
+
+//         member.define = function() {
+//             console.log(`${this.name} (${this.type}): ${this.cost}`)
+//         }
+
+//         return member;
+//     }
+// }
+
+// const SimpleMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$5';
+// }
+
+// const StandardMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$15';
+// }
+
+// const SuperMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$25';
+// }
+
+// const members = [];
+// const factory = new memberFactory();
+
+// members.push(factory.createMember('John Doe', 'simple'));
+// members.push(factory.createMember('Doe Doe', 'super'));
+// members.push(factory.createMember('John John', 'super'));
+// members.push(factory.createMember('Doe John', 'standard'));
+
+
+// // console.log(members);
+
+// members.forEach(function(member){
+//     member.define();
+// })
+
+//-------------------------------------------------------------------------------------------//
+
+function eventObeserver(){
+    this.observers = [];    
+}
+
+eventObeserver.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    },
+    unsubscribe: function(fn){
+        this.observers = this.observers.filter(function(item){
+            if(item !== fn){
+                return item;
             }
-
-        member.type = type;
-
-        member.define = function() {
-            console.log(`${this.name} (${this.type}): ${this.cost}`)
-        }
-
-        return member;
+        });
+        console.log(`You are now unsubsribed from ${fn.name}`)
+    },
+    fire: function() {
+        this.observers.forEach(function(item){
+            item.call();
+        })
     }
 }
 
-const SimpleMembership = function(name) {
-    this.name = name;
-    this.cost = '$5';
+const click = new eventObeserver();
+
+// Create event listeners
+
+document.querySelector('.sub-ms').addEventListener('click',function() {
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click',function() {
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click',function() {
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click',function() {
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click',function() {
+    click.fire();
+});
+
+// Click Handler
+const getCurMilliseconds = function() {
+    console.log(`Current milliseconds: ${new Date().getMilliseconds()}`)
 }
-
-const StandardMembership = function(name) {
-    this.name = name;
-    this.cost = '$15';
+const getCurSeconds = function() {
+    console.log(`Current Seconds: ${new Date().getSeconds()}`)
 }
-
-const SuperMembership = function(name) {
-    this.name = name;
-    this.cost = '$25';
-}
-
-const members = [];
-const factory = new memberFactory();
-
-members.push(factory.createMember('John Doe', 'simple'));
-members.push(factory.createMember('Doe Doe', 'super'));
-members.push(factory.createMember('John John', 'super'));
-members.push(factory.createMember('Doe John', 'standard'));
-
-
-// console.log(members);
-
-members.forEach(function(member){
-    member.define();
-})
